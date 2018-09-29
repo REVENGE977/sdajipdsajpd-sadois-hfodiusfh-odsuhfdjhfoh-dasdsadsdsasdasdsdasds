93661de3affa233e9ec17fc333843cfca4dd12a0
_ShaDoW_MaN_ <32039975+ShadwX57@users.noreply.github.com>
2018-09-29 18:57:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,62 +33,86 @@ client.on('ready', () => {
 
 
 
-client.on('message', async message => {  
-    if(message.content.startsWith(prefix + "sharejs")) {   
-      if(message.channel.name !== 'sharejs') return message.reply('يجب كتابه الامر في روم باسم sharejs')
-      await message.channel.send("**:writing_hand: ارسل الكود الان **").then(e => {
-      let filter = m => m.author.id === message.author.id
-      let lan = '';
-      let md = '';  
-      let br = '';  
-      let chaLan = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+const code = 'L!';
+
+client.on('message',async message => {
+    if(message.content.startsWith(code + "js")) {
+  if(!message.channel.guild) return message.reply(' ');
+    let rank = message.guild.member(message.author).roles.find('name', 'Developer');
+    if (!rank) return message.channel.send(':octagonal_sign: **| يجب ان تمتلك رتبة Developer  لأستخدام هذا الأمر.**');
+    let jscodes = message.guild.channels.find(`name`, "codes-js");
+    if(!jscodes) return message.channel.send(":x:لم اجد الروم الخاص بنشر الاكواد");
+      let filter = m => m.author.id === message.author.id;
+      let thisMessage;
+      let thisFalse;
+      message.channel.send(':pencil: **| من فضلك اكتب الكود الأن... :pencil2: **').then(msg => {
+  
+      message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 90000,
+        errors: ['time']
+      })
       .then(collected => {
-        lan = collected.first().content
-        collected.first().delete()
-  e.edit(`**:writing_hand: اكتب وصف الكود الان**`)
-  let chaMd = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
-  .then(co => {  
-    md = co.first().content
-          co.first().delete()  
-          e.edit(`**:writing_hand: ارسل المصدر او صانع الكود**`)
-  let br = message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })    
-  .then(col => {
-    br = col.first().content
-          col.first().delete()
-  e.edit("**جاري النشر ...**").then(b => {    
-          setTimeout(() => {
-    b.edit(`**تم النشر بنجاح**`)
-          },2000);
-  let gg = message.guild.channels.find('name', 'codes-js')
-  if(!gg) return message.reply('لا يوجد روم باسم codes-js')
-  if(gg) {
-  gg.send(`
-  @everyone | @here 
-  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-  ${message.guild.name}© :arrow_down:
-  ${lan}
-
-  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        collected.first().delete();
+        thisMessage = collected.first().content;
+        let boi;
+        msg.edit(':scroll: **| من فضلك اكتب وصف الكود الأن... :pencil2: **').then(msg => {
   
-  **وصف الكود:** ${md}
-  **تم النشر بواسطة:** ${message.author.tag} With ID ${message.author.id}
-  **المصدر / الشخص يلي صنع الكود:** ${br}
+            message.channel.awaitMessages(filter, {
+              max: 1,
+              time: 90000,
+              errors: ['time']
+            })
+            .then(collected => {
+              collected.first().delete();
+              boi = collected.first().content;
+              let boi2;
+              msg.edit(':man_in_tuxedo: **| من فضلك اكتب من صنع هذا الكود الأن... :pencil2: **').then(msg => {
   
-  
-  
-  
-  
-  `)
-  }
+                message.channel.awaitMessages(filter, {
+                  max: 1,
+                  time: 90000,
+                  errors: ['time']
+                })
+                .then(collected => {
+                  collected.first().delete();
+                boi2 = collected.first().content;
+        msg.edit(':shield: **| [ هل انت متأكد من نشر الكود؟ | [ نعم ] او [ لا**');
+   message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
+          max: 1,
+          time: 90000,
+          errors: ['time']
+        })
+        .then(collected => {
+          if(collected.first().content === 'لا') {
+            msg.delete();
+            message.delete();
+            thisFalse = false;
+          }
+          if(collected.first().content === 'نعم') {
+            if(thisFalse === false) return;
+            msg.edit(':dove: **| Done :white_check_mark:, تم بنجاح نشر كودك في روم الاكواد**');
+            collected.first().delete();
+            jscodes.send(`@everyone | @here
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+**Tesla Codes© :arrow_down:**            
+\`\`\`js
+${thisMessage}\`\`\`
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+**وصف الكود**: ${boi}
+**تم النشر بواسطة**: ${message.author}
+**المصدر / الشخص الذي صنع الكود**: ${boi2}`); 
+          }
+        }
+    );
+});
+      });
+    }
+      );
+    });
 }
-)}
-  )}
-)}
-      )}
-    )}
-})
-
-
+);
+      })}});
 
 
 
